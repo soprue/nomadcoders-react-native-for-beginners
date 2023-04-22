8838860254a9994cd3dc5ba0cb720f278dad7de2
soprue <soprue@naver.com>
2023-04-23 01:15:23 +0900

    Feat : 기본 레이아웃 작성

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -4,8 +4,15 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Hello! I made a RN App!</Text>
-            <StatusBar style="auto" />
+            <View style={styles.city}>
+                <Text style={styles.cityName}>Seoul</Text>
+            </View>
+            <View style={styles.weather}>
+                <View style={styles.day}>
+                    <Text style={styles.temp}>27</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+            </View>
         </View>
     );
 }
@@ -13,8 +20,30 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
+        backgroundColor: "tomato",
+    },
+    city: {
+        flex: 1,
         justifyContent: "center",
+        alignItems: "center",
+    },
+    cityName: {
+        fontSize: 48,
+        fontWeight: "600",
+    },
+    weather: {
+        flex: 3,
+    },
+    day: {
+        flex: 1,
+        alignItems: "center",
+    },
+    temp: {
+        marginTop: 50,
+        fontSize: 178,
+    },
+    description: {
+        marginTop: -30,
+        fontSize: 60,
     },
 });
