5bf717f86c0226ee04069e8ae3dc25b96dce1d0c
soprue <soprue@naver.com>
2023-04-23 11:55:57 +0900

    Feat : 위치 정보 가져오기

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,13 +1,39 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import { ScrollView, StyleSheet, Text, View, Dimensions } from "react-native";
+import * as Location from "expo-location";
 
 const { width: SCREEN_SIZE } = Dimensions.get("window");
 
 export default function App() {
+    const [city, setCity] = useState("Loading...");
+    const [location, setLocation] = useState();
+    const [ok, setOk] = useState(true);
+
+    const ask = async () => {
+        const { granted } = await Location.requestForegroundPermissionsAsync();
+        if (!granted) {
+            setOk(false);
+        }
+
+        const {
+            coords: { latitude, longitude },
+        } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+        const location = await Location.reverseGeocodeAsync(
+            { latitude, longitude },
+            { useGoogleMaps: false }
+        );
+        setCity(location[0].city);
+    };
+
+    useEffect(() => {
+        ask();
+    }, []);
+
     return (
         <View style={styles.container}>
             <View style={styles.city}>
-                <Text style={styles.cityName}>Seoul</Text>
+                <Text style={styles.cityName}>{city}</Text>
             </View>
             <ScrollView
                 pagingEnabled
