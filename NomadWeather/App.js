57851743d0708c332b445bd297f6a57ee54ee15f
soprue <soprue@naver.com>
2023-04-23 11:38:55 +0900

    Feat : ScrollView 적용

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View, Dimensions } from "react-native";
+
+const { width: SCREEN_SIZE } = Dimensions.get("window");
 
 export default function App() {
     return (
@@ -7,12 +9,33 @@ export default function App() {
             <View style={styles.city}>
                 <Text style={styles.cityName}>Seoul</Text>
             </View>
-            <View style={styles.weather}>
+            <ScrollView
+                pagingEnabled
+                horizontal
+                // indicatorStyle="white"
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={styles.weather}
+            >
                 <View style={styles.day}>
                     <Text style={styles.temp}>27</Text>
                     <Text style={styles.description}>Sunny</Text>
                 </View>
-            </View>
+
+                <View style={styles.day}>
+                    <Text style={styles.temp}>27</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+
+                <View style={styles.day}>
+                    <Text style={styles.temp}>27</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+
+                <View style={styles.day}>
+                    <Text style={styles.temp}>27</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+            </ScrollView>
         </View>
     );
 }
@@ -31,11 +54,9 @@ const styles = StyleSheet.create({
         fontSize: 48,
         fontWeight: "600",
     },
-    weather: {
-        flex: 3,
-    },
+    weather: {},
     day: {
-        flex: 1,
+        width: SCREEN_SIZE,
         alignItems: "center",
     },
     temp: {
