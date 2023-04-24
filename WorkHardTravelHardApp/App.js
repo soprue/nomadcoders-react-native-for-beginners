082224278047dbbfcf89c657e8921a3572cdae03
soprue <soprue@naver.com>
2023-04-24 15:30:08 +0900

    Feat : 기본 레이아웃 설정

--- WorkHardTravelHardApp/App.js
+++ WorkHardTravelHardApp/App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    TouchableHighlight,
+    TouchableWithoutFeedback,
+} from "react-native";
+import { theme } from "./colors";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <View style={styles.header}>
+                <TouchableOpacity>
+                    <Text style={styles.btnText}>Work</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.btnText}>Travel</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: theme.bg,
+        paddingHorizontal: 20,
+    },
+    header: {
+        justifyContent: "space-between",
+        flexDirection: "row",
+        marginTop: 100,
+    },
+    btnText: {
+        fontSize: 38,
+        fontWeight: "600",
+        color: "white",
+    },
 });
